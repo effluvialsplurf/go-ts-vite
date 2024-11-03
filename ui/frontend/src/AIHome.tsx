dfbffc6f1da01f67d716a0cdf5ba245b7e87749e
effluvialsplurf <eesowrk@gmail.com>
2024-11-03 17:01:09 -0500

    initial changes to the frontend app just to see how it works.

---
+++ ui/frontend/src/AIHome.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function AIHome() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>AI Home</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/aiHome.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default AIHome
